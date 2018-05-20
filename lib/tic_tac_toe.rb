e057a570f8264723c9501eaa51860e1ce6924a41
Gabriel Miranda <gabrielcm.br@gmail.com>
2018-05-20 14:16:26 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -59,8 +59,16 @@ class TicTacToe
     @board.count{|pos| pos == "X" || pos == "O"}
   end
   
-  def current_player(board)
+  def current_player
     turn_count % 2 == 0 ? "X" : "O"
+  end
+  
+  def won?
+    WIN_COMBINATIONS.each do |wincombo|
+      if ((@board[wincombo[0]] == "X" && @board[wincombo[1]] == "X" && @board[wincombo[2]] == "X") || (@board[wincombo[0]] == "O" && @board[wincombo[1]] == "O" && @board[wincombo[2]] == "O"))
+        return wincombo
+      end
+    return false
   end
   
   
