1dfeb9f0ce7d60e188d9fa39c4745833225d2c40
Gabriel Miranda <gabrielcm.br@gmail.com>
2018-05-20 14:31:35 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -96,4 +96,14 @@ class TicTacToe
     end
   end
   
+  def play
+    until (over?)
+      turn
+    end
+    if (won?)
+      puts "Congratulations #{winner}!"
+    else
+      puts "Cat's Game!"
+    end
+  end
 end
